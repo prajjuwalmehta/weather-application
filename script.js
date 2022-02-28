fc597ebb6890ea0b2a6819261340f0279d1d0cb1
prajjuwalmehta <100254819+prajjuwalmehta@users.noreply.github.com>
2022-02-28 20:03:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,65 @@
+let loc =document.getElementById("location");
+let tempicon=document.getElementById("temp-icon");
+let tempvalue=document.getElementById("temp-value");
+let climate =document.getElementById("climate");
+let iconfile;
+const searchInput=document.getElementById("search-input");
+const searchButton=document.getElementById("search-button");
+searchButton.addEventListener('click', (e)=>
+{
+e.preventDefault();
+getWeather(searchInput.value);
+searchInput.value='';
+});
+const getWeather=async (city)=>
+{
+    try{
+
+        const response= await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=dab3af44de7d24ae7ff86549334e45bd`,
+   
+            {mode: 'cors'}
+        );
+        const weatherData= await response.json();
+        console.log(weatherData);
+        const{name}=weatherData;
+        const{feels_like}=weatherData.main;
+        const{id,main}=weatherData.weather[0];
+        loc.textContent=name;
+        climate.textContent=main;
+        tempvalue.textContent=Math.round(feels_like-273);
+        if(id<233&&id>200)
+            {
+                tempicon.src="./icons/11n.png"
+            }
+            else if(id<322&&id>300)
+            {
+                tempicon.src="./icons/04d.png"
+            }
+            else if(id<532&&id>500)
+            {
+                tempicon.src="./icons/09n.png"
+            }
+            else if(id<623&&id>600)
+            {
+                tempicon.src="./icons/13d.png"
+            }
+            else if(id<782&&id>700)
+            {
+                tempicon.src="./icons/50d.png"
+            }
+            else if(id==800)
+            {
+                tempicon.src="./icons/01d.png"
+
+            }
+            else if(id>800)
+            {
+                tempicon.src="./icons/03d.png"
+            }
+    }
+catch(error)
+{
+    alert('city not found');
+}
+};
+
